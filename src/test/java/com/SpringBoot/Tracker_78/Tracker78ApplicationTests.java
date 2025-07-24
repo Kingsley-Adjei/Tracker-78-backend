41910c68f997b41ea681154f5cbb6791427d36a2
YUNGZEUS <kingkadjei002@email.com>
2025-07-24 13:19:18 +0000

    Prepare for Docker deployment

--- src/test/java/com/SpringBoot/Tracker_78/Tracker78ApplicationTests.java
+++ src/test/java/com/SpringBoot/Tracker_78/Tracker78ApplicationTests.java
@@ -2,9 +2,11 @@ package com.SpringBoot.Tracker_78;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class Tracker78ApplicationTests {
+@ActiveProfiles("test")
+public class Tracker78ApplicationTests {
 
 	@Test
 	void contextLoads() {
